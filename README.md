38e49285c0940c2a2ef0423c02b865ca9da785c2
JuanGarciaSantos <alu0101325583@ull.edu.es>
2022-04-26 21:02:35 +0100

    Done first practice

---
+++ README.md
@@ -0,0 +1,10 @@
+# Author: Juan García Santos
+## Python program to build the vocabulary
+### use example: python3 vocabulario.py
+#### Generates file vocabulario.txt containing all words from the COV_train.xlsx.
+### Transformations used:
+  - Tokenization
+  - Lower case everything
+  - Delete punctuators and emojis
+  - Delete hashtags, mentions, URL´s and HTML tags
+  - Lematization (Optional, it is calculated but not used)
---
+++ vocabulario.py
@@ -0,0 +1,62 @@
+import spacy
+nlp = spacy.load('en_core_web_sm')
+import openpyxl
+from pathlib import Path
+import numpy as np
+import re
+
+print("Reading the file COV_train.xlsx ...")
+
+xlsx_file = Path('COV_train.xlsx')
+wb_obj = openpyxl.load_workbook(filename=xlsx_file, read_only=True)
+sheet = wb_obj.active
+text = []
+for row in sheet.iter_rows(max_col=1):
+    for cell in row:
+        text.append(cell.value)
+
+# convert the text to a spacy document
+# all spacy documents are tokenized. You can access them using document[i]
+#document = nlp(text[0])
+
+print("Tokenizing every tweet and filtering tokens")
+
+words = []
+for row in text:
+  doc = nlp(row)
+  for token in doc:
+    word = token.text
+    url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', word)
+    mentions = re.findall('@\S+', word)
+    hashtags = re.findall('#\S+', word)
+    htmlTags = re.findall('\<[/]?\S+\>', word)
+    unicode = re.findall('[^a-z^\ ]', word)
+    spaces = re.findall('\s+', word)
+    #character = re.findall('\w+', word)
+    if not token.is_stop and not token.is_punct and not url and not mentions and not hashtags and not htmlTags and not unicode and not spaces and not word.isdigit():
+      if(word.islower()):
+        words.append(word)
+      else:
+        words.append(word.lower())
+
+print("Sorting and removing uniques from vocabulary ...")
+
+wordList = np.array(words)
+uniqueWords = np.unique(wordList)
+uniqueWords.sort()
+
+print("Dumping vocabulary into file ...")
+
+file = open("vocabulario.txt", "w+")
+file.write("Number of words: " + str(uniqueWords.size) + "\n")
+for word in uniqueWords:
+  file.write(word + "\n")
+file.close()
+
+#the good thing about spacy is a lot of things like lemmatization etc are done when you convert them to a spacy document `using nlp(text)`. You can access sentences using document.sents
+#list(document.sents)[0]
+
+# lemmatized words can be accessed using document[i].lemma_ and you can check 
+# if a word is a stopword by checking the `.is_stop` attribute of the word.
+# here I am extracting the lemmatized form of each word provided they are not a stop word
+#lemmas = [token.lemma_ for token in document if not token.is_stop]
