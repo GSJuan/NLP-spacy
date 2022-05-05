1b6ec1858b3b02b3d0de7a40a7930457ffd808c6
JuanGarciaSantos <alu0101325583@ull.edu.es>
2022-05-05 22:39:09 +0100

    changed probability calculus

--- aprendizaje.py
+++ aprendizaje.py
@@ -27,16 +27,6 @@ vocabulary = np.array(readFile("vocabulario.txt"))
 
 print("Printing basic info into model files ...")
 
-file = open("modelo_lenguaje_P.txt", "w+", encoding='utf-8')
-file.write("Número de documentos (tweets) del corpus: " + str(positiveTweets.size))
-file.write("\nNúmero de palabras del corpus: " + str(positiveWords.size))
-file.close()
-
-file = open("modelo_lenguaje_N.txt", "w+", encoding='utf-8')
-file.write("Número de documentos (tweets) del corpus: " + str(negativeTweets.size))
-file.write("\nNúmero de palabras del corpus: " + str(negativeWords.size))
-file.close()
-
 #negativo
 #Numero de documentos (tweets) del corpus: 15398
 #Número de palabras del corpus: 201117
@@ -45,36 +35,51 @@ file.close()
 #Numero de documentos (tweets) del corpus: 18046
 #Número de palabras del corpus: 235230
 
-print("Computing frequencies ...")
-
 positiveList = Counter(positiveWords.tolist())
 negativeList = Counter(negativeWords.tolist())
 
-posFile = open("modelo_lenguaje_P.txt", "a+", encoding='utf-8')
-negFile = open("modelo_lenguaje_N.txt", "a+", encoding='utf-8')
+vocabSize = vocabulary.size
+
+numPosWords = len(positiveList.items())
+numNegWords = len(negativeList.items())
+
+""""
+numPosWords = positiveWords.size
+numNegWords = negativeWords.size
+"""
+
+posFile = open("modelo_lenguaje_P.txt", "w+", encoding='utf-8')
+posFile.write("Número de documentos (tweets) del corpus: " + str(positiveTweets.size))
+posFile.write("\nNúmero de palabras del corpus: " + str(numPosWords))
+
+negFile = open("modelo_lenguaje_N.txt", "w+", encoding='utf-8')
+negFile.write("Número de documentos (tweets) del corpus: " + str(negativeTweets.size))
+negFile.write("\nNúmero de palabras del corpus: " + str(numNegWords))
+
+print("Computing frequencies ...")
 
 unkPos = 0
 unkNeg = 0
 for word in vocabulary:
  
-  if (word in positiveList) and positiveList[word]  > 2:
+  if (word in positiveList) and positiveList[word]  > 5:
     positiveFrequency = positiveList[word] 
-    probabilityPos = ln(positiveFrequency + 1) - ln(positiveWords.size + vocabulary.size)
+    probabilityPos = ln(positiveFrequency + 1) - ln(numPosWords + vocabSize)
     posFile.write("\nPalabra: " + word + " Frec: " + str(positiveFrequency) + " lnProb: " + str(round(probabilityPos, 2)))
   else:
     unkPos += 1
   
-  if (word in negativeList) and negativeList[word] > 2:
+  if (word in negativeList) and negativeList[word] > 5:
     negativeFrequency = negativeList[word]
-    probabilityNeg = ln(negativeFrequency + 1) - ln(negativeWords.size + vocabulary.size)
+    probabilityNeg = ln(negativeFrequency + 1) - ln(numNegWords + vocabSize)
     negFile.write("\nPalabra: " + word + " Frec: " + str(negativeFrequency) + " lnProb: " + str(round(probabilityNeg, 2)))
   else:
     unkNeg += 1
 
-unkPosProb = ln(unkPos + 1) - ln(positiveWords.size + vocabulary.size)
+unkPosProb = ln(unkPos + 1) - ln(numPosWords + vocabSize)
 posFile.write("\nPalabra: UNK" + " Frec: " + str(unkPos) + " lnProb: " + str(round(unkPosProb, 2)))
 
-unkNegProb = ln(unkNeg + 1) - ln(negativeWords.size + vocabulary.size)
+unkNegProb = ln(unkNeg + 1) - ln(numNegWords + vocabSize)
 negFile.write("\nPalabra: UNK" + " Frec: " + str(unkNeg) + " lnProb: " + str(round(unkNegProb, 2)))
 
 posFile.close()
